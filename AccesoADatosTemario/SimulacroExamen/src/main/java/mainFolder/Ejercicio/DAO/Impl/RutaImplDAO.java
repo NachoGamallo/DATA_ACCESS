caeeb6f8f9d4457a42b731ddc606820307b20533
NASHE\shote <nachogamallolafon123@gmail.com>
2026-01-21 15:17:46 +0100

    Simulacro examen, falta DAO

--- AccesoADatosTemario/SimulacroExamen/src/main/java/mainFolder/Ejercicio/DAO/Impl/RutaImplDAO.java
+++ AccesoADatosTemario/SimulacroExamen/src/main/java/mainFolder/Ejercicio/DAO/Impl/RutaImplDAO.java
@@ -1,7 +1,10 @@
 package mainFolder.Ejercicio.DAO.Impl;
 
 import mainFolder.Ejercicio.DAO.Interface.IRutaDAO;
+import mainFolder.Ejercicio.config.GestionSesiones;
 import mainFolder.Ejercicio.module.Ruta;
+import mainFolder.Ejercicio.module.Usuario;
+import org.hibernate.Session;
 
 import java.util.List;
 
@@ -9,16 +12,28 @@ public class RutaImplDAO implements IRutaDAO {
 
     @Override
     public Ruta findById(Integer id) {
-        return null;
+        try (Session session = GestionSesiones.getSessionFactory().openSession()){
+
+            return session.find(Ruta.class,id);
+
+        }
     }
 
     @Override
     public List<Ruta> findAll() {
-        return List.of();
+
+        try (Session session = GestionSesiones.getSessionFactory().openSession()){
+
+            return session.createQuery("from Ruta",Ruta.class).list();
+
+        }
+
     }
 
     @Override
     public void create(Ruta ruta) {
+
+        
 
     }
 
